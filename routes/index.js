ace7344aceb8e0bdc27807b310523e6096a83768
hieuphansipher <hieu.phan@sipher.xyz>
2022-07-03 00:32:54 +0700

    update frontend

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,14 @@ router.get('/homepage', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
+router.get('/manager-neccesary', function (req, res, next) {
+    res.render('manager_neccessary', { title: 'manager neccessary' });
+});
+
+router.get('/manager-neccesary-packet', function (req, res, next) {
+    res.render('manager_neccessary_packet', { title: 'manager neccessary packet' });
+});
+
 router.use(userRouter);
 router.use(adminRouter);
 
